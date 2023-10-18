56797a111ba78edef7e844ea59a9b7b3f5925e6a
isidorae <mariaisidoraerrazuriz@gmail.com>
2023-10-18 19:29:23 -0300

    cors+guia rutas

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 const app = express()
 const morgan = require('morgan');
+const cors = require('cors')
 
 const router = require('./src/routers/routers')
 
@@ -10,11 +11,24 @@ const PORT = process.env.PORT
 const MONGO_URI = process.env.MONGO_URI
 const apiUrl = process.env.API_URL
 
+app.use(cors())
 app.use(express.json());
 app.use(morgan('tiny'));
 
 
 app.use(`${apiUrl}/`, router)
+app.get('/', (req, res) => {
+    res.send(
+        {products: "https://ratclub.onrender.com/rat-club-api/v1/products",
+        categories: "https://ratclub.onrender.com/rat-club-api/v1/categories",
+        foodProducts: "https://ratclub.onrender.com/rat-club-api/v1/products/collection/alimentos",
+        accessoryProducts:"https://ratclub.onrender.com/rat-club-api/v1/products/collection/accesorios",
+        toyProducts: "https://ratclub.onrender.com/rat-club-api/v1/products/collection/juguetes",
+        homeProducts: "https://ratclub.onrender.com/rat-club-api/v1/products/collection/hogar",
+        featuredProducts: "https://ratclub.onrender.com/rat-club-api/v1/products/featured/all"
+
+    })
+})
 
 
 mongoose.connect(MONGO_URI).then(() => console.log('connected!'));
